9d20ba337980fe9af1f6630fe4922400c7b0ecc6
LaodeMFauzan <fauzanlaode@yahoo.co.id>
2018-08-31 09:40:59 +0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to my new Gatsby site.</p>
+    <p>My name is Fauzan</p>
+    <p>I'm from Indonesia</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
